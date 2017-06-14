73913569a27d4d1d25f1357a6614f9b5cb7c9a9d
Dirk Burger <dirk@inspiron-1525-1.lan>
2017-06-14 23:02:00 +0100

    initial

---
+++ JWLMerge/test.py
@@ -0,0 +1,33 @@
+# Two 'userData.db' sqlite3 databases needed
+# 'userDataLarge.db' = Main database
+# 'userDataSmall.db' = secondary database to be merged in
+
+import sqlite3
+
+largeResults = []
+smallResults = []
+
+connLarge = sqlite3.connect('/home/dirk/Documents/JWLDatabases/userDataLarge.db')
+connSmall = sqlite3.connect('/home/dirk/Documents/JWLDatabases/userDataSmall.db')
+
+cursorLarge = connLarge.cursor()
+cursorSmall = connSmall.cursor()
+
+for rowLarge in cursorLarge.execute('SELECT BookNumber, ChapterNumber, Title FROM Location WHERE BookNumber IS NOT NULL'):
+    largeResults += [rowLarge]
+for rowSmall in cursorSmall.execute('SELECT BookNumber, ChapterNumber, Title FROM Location WHERE BookNumber IS NOT NULL'):
+    smallResults += [rowSmall]
+
+# similarity between
+similarResults =  set(smallResults).intersection(largeResults)
+
+# get Location record of similar results in userDataLarge.db
+for similarItem in similarResults:
+    title = (similarItem[2],)
+    cursorLarge.execute('SELECT * FROM Location WHERE Title=?', title)
+    print cursorLarge.fetchall()
+
+
+
+connLarge.close()
+connSmall.close()
